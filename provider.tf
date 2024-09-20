46c13ceca5f48764bc7030a66a0e3eed974c7798
charmingruby <mrrubycanal3105@gmail.com>
2024-09-20 18:44:22 -0300

    feat: adds s3 backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.48.0"
     }
   }
+
+  backend "s3" {
+    bucket = "kuba-iac"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
